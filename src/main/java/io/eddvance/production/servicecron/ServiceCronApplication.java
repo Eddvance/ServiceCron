e73732e1f414651cc8fafed317aae57ebf00fb3b
Edouard Leroy <leroyedouard@protonmail.com>
2025-05-05 13:51:06 +0200

    Initial commit

---
+++ src/main/java/io/eddvance/production/servicecron/ServiceCronApplication.java
@@ -0,0 +1,13 @@
+package io.eddvance.production.servicecron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/eddvance/production/servicecron/ServiceCronApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddvance.production.servicecron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
