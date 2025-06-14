8da597626b7a51dc9b16e8e0187522068ff52121
Edouard Leroy <leroyedouard@protonmail.com>
2025-06-14 16:08:27 +0200

    modification de LowCarb pour appeler ServiceCron au lieu de LowCarbPower, divers bugs, add loadbalancer et caffeine non fonctionnel

--- src/main/java/io/eddvance/production/servicecron/rate/RateScheduler.java
+++ src/main/java/io/eddvance/production/servicecron/rate/RateScheduler.java
@@ -12,7 +12,7 @@ public class RateScheduler {
         this.rateHistoryService = rateHistoryService;
     }
 
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     public void collectHourlyRate() {
         rateHistoryService.saveCurrentRate().subscribe(
                 rate -> System.out.println("✅ Tarif: " + rate.getRate()),
